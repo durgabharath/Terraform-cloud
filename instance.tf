fb68fbcfdf43adf821684444bdf250d6f644423b
durgabharath <126495484+durgabharath@users.noreply.github.com>
2023-03-24 21:25:55 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -6,7 +6,7 @@ secret_key=var.AWS_SECRET_KEY
 
 resource "aws_instance" "web" { 
 ami           = "ami-02dc8ad50da58fffd" 
-instance_type = "t3.micro" 
+instance_type = "t2.micro" 
 tags = { 
 Name = "HelloWorld" 
 } 
